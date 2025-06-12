1798dc8e4b442ae79fca188e05e5c95e5091f39f
Lordwick5 <Prashantchy96@gmail.com>
2025-06-13 04:45:36 +0545

    updated code

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ function rollDice() {
     for (let i = 0; i < numOfDice; i++) {
         const value = Math.floor(Math.random() * 6) + 1;
         values.push(value);
+        images.push(`<img src = "dice/${value}.png" alt="Dice ${value}">`);
     }
 
-    
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
 }
